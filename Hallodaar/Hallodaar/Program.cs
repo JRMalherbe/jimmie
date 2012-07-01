3aa1d2271a96476eaec40add196731dc9a407b6d
Hans Malherbe <hans.malherbe@gmail.com>
2012-07-01 11:43:44 +0200

    Konsole hallo daar

---
+++ Hallodaar/Hallodaar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hallodaar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo daar");
+        }
+    }
+}
